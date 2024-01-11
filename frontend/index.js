66448fee2146401a132b6af1436fb5627d3dacfc
Yan Victor <ybritogomes@gmail.com>
2024-01-11 16:59:03 -0300

    feat: now refreshing all cars after upload a car and init refresh cars pull

--- frontend/index.js
+++ frontend/index.js
@@ -24,42 +24,44 @@ import DOM from "./DOM.js"
       })
     }
 
-    function addLastAddedCar(car) {
+    function refreshCars(cars) {
       const $tableCars = doc.querySelector('[data-js="tableCars"]');
 
-      const tableRow = doc.createElement('tr');
+      cars.forEach((car) => {
+        const tableRow = doc.createElement('tr');
+  
+        const tableDataImg = doc.createElement('td');
+        const img = doc.createElement('img');
+        img.setAttribute('style', `width: 250px; height: 250px;`);
+        img.setAttribute('src', `${car.imageLink}`);
+        tableDataImg.appendChild(img);
+  
+        const tableDataMarca = doc.createElement('td');
+        tableDataMarca.appendChild(doc.createTextNode(`${car.marca}`));
+  
+        const tableDataAno = doc.createElement('td');
+        tableDataAno.appendChild(doc.createTextNode(`${car.ano}`));
+  
+        const tableDataPlaca = doc.createElement('td');
+        tableDataPlaca.appendChild(doc.createTextNode(`${car.placa}`));
+  
+        const tableDataCor = doc.createElement('td');
+        tableDataCor.appendChild(doc.createTextNode(`${car.cor}`));
+        
+        const tableButton = doc.createElement('td');
+        const button = doc.createElement('button');
+        const trashVector = doc.createElement('img');
+        trashVector.setAttribute('src', './vectors/trash-2.svg');
+        button.appendChild(trashVector);
+        tableButton.appendChild(button);
+  
+        tableRow.append(tableDataImg, tableDataMarca, tableDataAno, tableDataPlaca, tableDataCor, tableButton);
+        $tableCars.appendChild(tableRow);
 
-      const tableDataImg = doc.createElement('td');
-      const img = doc.createElement('img');
-      img.setAttribute('style', `width: 250px; height: 250px;`);
-      img.setAttribute('src', `${car.imageLink}`);
-      tableDataImg.appendChild(img);
-
-      const tableDataMarca = doc.createElement('td');
-      tableDataMarca.appendChild(doc.createTextNode(`${car.marca}`));
-
-      const tableDataAno = doc.createElement('td');
-      tableDataAno.appendChild(doc.createTextNode(`${car.ano}`));
-
-      const tableDataPlaca = doc.createElement('td');
-      tableDataPlaca.appendChild(doc.createTextNode(`${car.placa}`));
-
-      const tableDataCor = doc.createElement('td');
-      tableDataCor.appendChild(doc.createTextNode(`${car.cor}`));
-      
-      const tableButton = doc.createElement('td');
-      const button = doc.createElement('button');
-      const trashVector = doc.createElement('img');
-      trashVector.setAttribute('src', './vectors/trash-2.svg');
-      button.appendChild(trashVector);
-      tableButton.appendChild(button);
-
-      tableRow.append(tableDataImg, tableDataMarca, tableDataAno, tableDataPlaca, tableDataCor, tableButton);
-      $tableCars.appendChild(tableRow);
-
-      button.addEventListener('click', (e) => {
-        e.preventDefault();
-        tableRow.parentNode.removeChild(tableRow);
+        button.addEventListener('click', (e) => {
+          e.preventDefault();
+          tableRow.parentNode.removeChild(tableRow);
+        })
       })
     }
 
@@ -69,17 +71,23 @@ import DOM from "./DOM.js"
 
         await uploadCar();
 
-        fetch('http://localhost:3000/car').then((response) => response.json()).then((response) => addLastAddedCar(response[response.length - 1]));
+        app().fetchCars();
       }
 
       doc.querySelector('[data-js="submitForm"]').addEventListener('click', submitHandler);
 
-      fetchCompanyInfo();
+      app().fetchCompanyInfo();
+
+      app().fetchCars();
     }
 
     function fetchCompanyInfo() {
       let fetchApi = fetch('company.json').then((response) => response.json()).then((companyInfo) => assignCompanyInfo(companyInfo));
       fetchApi.catch((e) => console.error(`Erro: ${e}`))
+    }
+
+    function fetchCars() {
+      fetch('http://localhost:3000/car').then((response) => response.json()).then((response) => refreshCars(response));
     }
 
     function assignCompanyInfo(companyInfo) {
@@ -93,8 +101,8 @@ import DOM from "./DOM.js"
     return {
       "init": init,
       "uploadCar": uploadCar,
-      "addLastAddedCar": addLastAddedCar,
-      "fetchCompanyInfo": fetchCompanyInfo
+      "fetchCompanyInfo": fetchCompanyInfo,
+      "fetchCars": fetchCars
     }
   }
 })(document)
